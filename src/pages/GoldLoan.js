c2e0c4a5bcc6431eddeac58247ef8a49963a6e30
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-15 05:06:30 +0000

    Gold loan pagge added

---
+++ src/pages/GoldLoan.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+const GoldLoan = () => {
+  return (
+    <section class="about section" style={{ paddingTop: "70px" }}>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9 col-xs-9">
+            <div class="about-content">
+              <h3>
+                Gold <span>Loan</span>
+              </h3>
+              <div class="text">
+                <p>
+                  Pledge your Gold Ornaments or Jewellery and avail the Highest
+                  Amount per gram of Gold. Depending on the net weight and
+                  purity of the gold, cash will be disbursed quickly.
+                  <br />
+                  We have various schemes that will suit for your needs. Please
+                  visit your nearest branch of Chemmanur Credits to avail Gold
+                  Loan.
+                  <br />
+                  - Interest will be charged @12.00%
+                  <br />
+                  - Rate of Gold of 22 Carrat as per market rate.
+                  <br />
+                  - Bank is financing against gold ornaments only.
+                  <br />
+                  - Valuation charges will be 0.25% of loan amount.
+                  <br />
+                  - Processing charges will be 0.50% of loan amount.
+                  <br />
+                  - Loan can be granted @50% of the value of Gold Jewellery as
+                  per valuation.
+                  <br />
+                </p>
+                <img src="/assets/img/gold.jpg" alt="Gold Loan" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-xs-3">
+            <ul class="blog-cat">
+              <li>
+                <a href="/home-loan.html">Home loan</a>
+              </li>
+              <li>
+                <a href="/personal-loan.html">Personal Loans</a>
+              </li>
+              <li>
+                <a href="/loans-against-property.html">
+                  Loans Against Property
+                </a>
+              </li>
+              <li>
+                <a href="/commercial-property-loans.html">
+                  Commercial Property Loans
+                </a>
+              </li>
+              <li>
+                <a href="/business-loan.html">Business Loan</a>
+              </li>
+              <li>
+                <a href="/vehicle-loan.html">Vehicle loan</a>
+              </li>
+              <li class="active">
+                <a href="/gold-loan.html">Gold loan</a>
+              </li>
+              <li>
+                <a href="/agriculture-loan.html">Agriculture Loan</a>
+              </li>
+              <li>
+                <a href="/education-loan.html">Education Loan</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GoldLoan;
