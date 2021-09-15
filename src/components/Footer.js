871d62bdcc3955a34ebce1389562f48e066641cc
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-15 04:02:07 +0000

    Header Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      {/* <!-- Footer Area --> */}
+      <footer class="footer">
+        {/* <!-- Copyright --> */}
+        <div class="copyright">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="copyright-content">
+                  <p>
+                    <i class="fa fa-copyright"></i>2014-2021 | Mahalaxmi
+                    Financial Services All Rights Reserved.
+                  </p>
+                </div>
+                <div class="design-credits">
+                  <a href="/credits.html">Designed By: UnRuffled Feathers</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <!-- End Copyright --> */}
+      </footer>
+      {/* <!-- End Footer Area --> */}
+    </>
+  );
+};
+
+export default Footer;
